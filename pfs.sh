3e5c86f9d745b2357d2b0fad80cf4d94c24735b8
heropoo <aiyouyou1000@163.com>
2021-05-01 14:56:29 +0800

    password

--- pfs.sh
+++ pfs.sh
@@ -8,6 +8,7 @@
 #########################################################################
 
 direc=`dirname $0`
+passwd_file="$direc/password"
 function color(){
     blue="\033[0;36m"
     red="\033[0;31m"
@@ -47,23 +48,26 @@ while [ True ];do
 
     echo "序号 |       主机      | 说明"
     underline
-    awk 'BEGIN {FS=":"} {printf("\033[0;31m% 3s \033[m | %15s | %s\n",$1,$2,$6)}' $direc/password
+    # awk 'BEGIN {FS=":"} {printf("\033[0;31m% 3s \033[m | %15s | %s\n",$1,$2,$6)}' $direc/password
+    awk '{if(!NF || /^#/){next}} BEGIN {FS=":"} {printf("\033[0;31m% 3s \033[m | %s@%s:%s | %s\n",$1,$4,$2,$3,$6)}' $passwd_file
     underline
     read -p '[*] 选择主机: ' number
-    pw="$direc/password"
-    ipaddr=$(awk -v num=$number 'BEGIN {FS=":"} {if($1 == num) {print $2}}' $pw)
-    port=$(awk -v num=$number 'BEGIN {FS=":"} {if($1 == num) {print $3}}' $pw)
-    username=$(awk -v num=$number 'BEGIN {FS=":"} {if($1 == num) {print $4}}' $pw)
-    passwd=$(awk -v num=$number 'BEGIN {FS=":"} {if($1 == num) {print $5}}' $pw)
+    #pw="$direc/password"
+    ipaddr=$(awk -v num=$number 'BEGIN {FS=":"} {if($1 == num) {print $2}}' $passwd_file)
+    port=$(awk -v num=$number 'BEGIN {FS=":"} {if($1 == num) {print $3}}' $passwd_file)
+    username=$(awk -v num=$number 'BEGIN {FS=":"} {if($1 == num) {print $4}}' $passwd_file)
+    passwd=$(awk -v num=$number 'BEGIN {FS=":"} {if($1 == num) {print $5}}' $passwd_file)
 
     case $number in
         [0-9]|[0-9][0-9]|[0-9][0-9][0-9])
-            echo $passwd | grep -q ".ssh"
-            RETURN=$?
-            if [[ $RETURN == 0 ]];then
-                ssh -i $passwd $username@$ipaddr -p $port
+            #echo $passwd | grep -q ".pem$"
+            #RETURN=$?
+            if [ -f "$passwd" ];then
+                #echo "$passwd 文件存在"
                 echo "ssh -i $passwd $username@$ipaddr -p $port"
+                ssh -i $passwd $username@$ipaddr -p $port
             else
+                #echo "$passwd 文件不存在"
                 expect -f $direc/ssh_login.exp $ipaddr $username $passwd $port
             fi
         ;;
